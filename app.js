a699ab5c587b13db129df3623a06de451be0585b
Taha S. ASAN <tahaasann@gmail.com>
2022-10-31 08:37:55 +0300

    Using Template Engine

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) => {
-
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+//ROUTES
+app.get('/home', (req, res) => {
+  
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  
+  res.render('add');
 });
 
 const port = 3000;
